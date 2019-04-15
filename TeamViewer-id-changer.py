29818b09cde72f0210424dfeae4b2262d2267625
phantom <623601391@qq.com>
2019-04-15 08:59:06 +0800

    feat: support python3+

--- TeamViewer-id-changer.py
+++ TeamViewer-id-changer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python 
 
-#coding:utf-8
+# coding:utf-8
 import sys
 import os
 import glob
@@ -17,37 +17,39 @@ TeamViewer ID Changer for MAC OS
 
 if platform.system() != 'Darwin':
     print('This script can be run only on MAC OS.')
-    sys.exit();
+    sys.exit()
 
 if os.geteuid() != 0:
     print('This script must be run form root.')
-    sys.exit();
+    sys.exit()
 
-if os.environ.has_key('SUDO_USER'):
+if 'SUDO_USER' in os.environ:
     USERNAME = os.environ['SUDO_USER']
     if USERNAME == 'root':
-       print('Can not find user name. Run this script via sudo from regular user')
-       sys.exit();
+        print('Can not find user name. Run this script via sudo from regular user')
+        sys.exit()
 else:
     print('Can not find user name. Run this script via sudo from regular user')
-    sys.exit();
+    sys.exit()
 
-HOMEDIRLIB = '/Users/' + USERNAME  + '/Library/Preferences/'
-GLOBALLIB  =  '/Library/Preferences/'
+HOMEDIRLIB = '/Users/' + USERNAME + '/Library/Preferences/'
+GLOBALLIB = '/Library/Preferences/'
 
 CONFIGS = []
+
 
 # Find config files
 
 def listdir_fullpath(d):
     return [os.path.join(d, f) for f in os.listdir(d)]
 
+
 for file in listdir_fullpath(HOMEDIRLIB):
     if 'teamviewer'.lower() in file.lower():
         CONFIGS.append(file)
 
 if not CONFIGS:
-    print ('''
+    print('''
 There is no TemViewer configs found.
 Maybe you have deleted it manualy or never run TeamViewer after installation.
 Nothing to delete.
@@ -56,7 +58,7 @@ Nothing to delete.
 else:
     print("Configs found:\n")
     for file in CONFIGS:
-        print file
+        print(file)
 
     print('''
 This files will be DELETED permanently.
@@ -69,15 +71,15 @@ All TeamViewer settings will be lost
             os.remove(file)
         except:
             print("Cannot delete config files. Permission denied?")
-            sys.exit();
+            sys.exit()
     print("Done.")
 
 # Find binaryes
 
 TMBINARYES = [
-'/Applications/TeamViewer.app/Contents/MacOS/TeamViewer',
-'/Applications/TeamViewer.app/Contents/MacOS/TeamViewer_Service',
-'/Applications/TeamViewer.app/Contents/Helpers/TeamViewer_Desktop',
+    '/Applications/TeamViewer.app/Contents/MacOS/TeamViewer',
+    '/Applications/TeamViewer.app/Contents/MacOS/TeamViewer_Service',
+    '/Applications/TeamViewer.app/Contents/Helpers/TeamViewer_Desktop',
 ]
 
 for file in TMBINARYES:
@@ -85,40 +87,43 @@ for file in TMBINARYES:
         pass
     else:
         print("File not found: " + file)
-        print ("Install TeamViewer correctly")
-        sys.exit();
+        print("Install TeamViewer correctly")
+        sys.exit()
+
 
 # Patch files
 
-def idpatch(fpath,platf,serial):
+def idpatch(fpath, platf, serial):
     file = open(fpath, 'r+b')
     binary = file.read()
     PlatformPattern = "IOPlatformExpert.{6}"
-    SerialPattern =  "IOPlatformSerialNumber%s%s%sUUID"
+    SerialPattern = "IOPlatformSerialNumber%s%s%sUUID"
 
     binary = re.sub(PlatformPattern, platf, binary)
-    binary = re.sub(SerialPattern % (chr(0), "[0-9a-zA-Z]{8,8}", chr(0)), SerialPattern%(chr(0), serial, chr(0)), binary)
+    binary = re.sub(SerialPattern % (chr(0), "[0-9a-zA-Z]{8,8}", chr(0)), SerialPattern % (chr(0), serial, chr(0)),
+                    binary)
 
-    file = open(fpath,'wb').write(binary)
+    file = open(fpath, 'wb').write(binary)
     return True
+
 
 def random_generator(size=8, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
+
 RANDOMSERIAL = random_generator()
 RANDOMPLATFORM = "IOPlatformExpert" + random_generator(6)
 
-
 for file in TMBINARYES:
-        try:
-            idpatch(file,RANDOMPLATFORM,RANDOMSERIAL)
-        except:
-            print "Error: can not patch file " + file
-            print "Wrong version?"
-            sys.exit();
+    try:
+        idpatch(file, RANDOMPLATFORM, RANDOMSERIAL)
+    except:
+        print("Error: can not patch file " + file)
+        print("Wrong version?")
+        sys.exit()
 
-print "PlatformDevice: " + RANDOMPLATFORM
-print "PlatformSerial: " + RANDOMSERIAL
+print("PlatformDevice: " + RANDOMPLATFORM)
+print("PlatformSerial: " + RANDOMSERIAL)
 
 print('''
 ID changed sucessfully.
